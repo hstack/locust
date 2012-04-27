c672fd396f7be18e008b60c4fe361d3d273b3c71
Adrian Muraru <amuraru@adobe.com>
2012-04-27 15:22:10 +0300

    Fixed pickled object to avoid different python version mismatch

--- locust/rpc/__init__.py
+++ locust/rpc/__init__.py
@@ -7,4 +7,8 @@ except ImportError:
     warnings.warn("WARNING: Using pure Python socket RPC implementation instead of zmq. This will not affect you if your not running locust in distributed mode, but if you are, we recommend you to install the python packages: pyzmq and gevent-zeromq")
     import socketrpc as rpc
 
-Message = namedtuple("Message", ["type", "data", "node_id"])
+class Message:
+        def __init__(self, type, data, node_id):
+                self.type=type
+                self.data=data
+                self.node_id=node_id
